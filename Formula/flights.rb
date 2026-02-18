840de87f7b3ce4ec37ab36050f00997160e0af7f
BrewTheFox <157254614+BrewTheFox@users.noreply.github.com>
2026-02-18 16:11:33 -0500

    Enhance Flights formula with binary option and resource

--- Formula/flights.rb
+++ Formula/flights.rb
@@ -1,17 +1,28 @@
 class Flights < Formula
   desc "fox-lights cli (CLI made in Rust for tuya v3.3 Light Bulbs). A.K.A my OpenClaw server does not support python so I can't just use tinytuya."
-  homepage "https://github.com/BrewTheFox/flights"
+  homepage "https://github.com/BrewTheFox/flights"  
   url "https://github.com/BrewTheFox/flights/archive/refs/tags/1.0.0.tar.gz"
   sha256 "3c477803314517cf4604be8b7368d90ff358852342e48bba6c9cbd5dcf63b325"
   license "Unlicense"
+  option "with-binary", "Download pre-compiled binary from github releases."
+  
+  depends_on "rust" => :build if build.without? "binary"
 
-  depends_on "rust" => :build
-
+  resource "binary" do
+    url "https://github.com/BrewTheFox/flights/releases/download/1.0.0/flights-linux.tar.gz"
+    sha256 "033216ebc89d9f620a57dfd3b94302db6e776bfd74c026b507e8833f47adf536"
+  end
+    
   def install
-    system "cargo", "install", *std_cargo_args
+    if build.with? "binary"
+      bin.install "flights"
+    end
+    else
+      system "cargo", "install", *std_cargo_args
+    end
   end
 
   test do
-    assert_match "flights", shell_output("#{bin}/flights --help")
+    system "#{bin}/flights", "--help"
   end
 end
