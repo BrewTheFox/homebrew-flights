cc0f3708454bca4b9e350044e830c2700ab89870
BrewTheFox <157254614+BrewTheFox@users.noreply.github.com>
2026-02-18 16:15:30 -0500

    Refactor Flights class description and install method

--- Formula/flights.rb
+++ Formula/flights.rb
@@ -1,22 +1,24 @@
 class Flights < Formula
-  desc "fox-lights cli (CLI made in Rust for tuya v3.3 Light Bulbs). A.K.A my OpenClaw server does not support python so I can't just use tinytuya."
-  homepage "https://github.com/BrewTheFox/flights"  
+  desc "fox-lights cli (CLI made in Rust for tuya v3.3 Light Bulbs)"
+  homepage "https://github.com/BrewTheFox/flights"
   url "https://github.com/BrewTheFox/flights/archive/refs/tags/1.0.0.tar.gz"
   sha256 "3c477803314517cf4604be8b7368d90ff358852342e48bba6c9cbd5dcf63b325"
   license "Unlicense"
+
   option "with-binary", "Download pre-compiled binary from github releases."
-  
+
   depends_on "rust" => :build if build.without? "binary"
 
   resource "binary" do
     url "https://github.com/BrewTheFox/flights/releases/download/1.0.0/flights-linux.tar.gz"
     sha256 "033216ebc89d9f620a57dfd3b94302db6e776bfd74c026b507e8833f47adf536"
   end
-    
+
   def install
     if build.with? "binary"
-      bin.install "flights"
-    end
+      resource("binary").stage do
+        bin.install "flights"
+      end
     else
       system "cargo", "install", *std_cargo_args
     end
